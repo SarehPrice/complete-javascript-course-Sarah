59d603c8ad8695216aad4f46749077ae2233dc9d
Sarah Price <48076969+SarehPrice@users.noreply.github.com>
2020-07-01 21:39:48 -0500

    Challenge 3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -281,3 +281,27 @@ GOOD LUCK 😀
 
 // var isDesigner = Ben.indexOf('designer') === -1 ? 'Ben is NOT a designer' : 'Ben IS a designer';
 // console.log(isDesigner);
+
+function tipCalculator(bill) {
+	var percentage;
+	if (bill < 50) {
+		percentage = .2;
+	} else if (bill >= 50 && bill < 200) {
+		percentage = .15;
+	} else {
+		percentage = .1;
+	}
+	return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]), tipCalculator(bills[1]), tipCalculator(bills[2]);
+var finalValues = [bills[0] + tips[0], bills[1] + tips[1],bills[2] + tips[2]];
+
+console.log(tips, finalValues);
+
+
+
+
+
+
