21c26379514cebd8a0125b0c289eb7d64519d8ec
Sarah Price <48076969+SarehPrice@users.noreply.github.com>
2020-08-25 22:35:52 -0500

    Finished Chapter 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -408,3 +408,82 @@ GOOD LUCK 😀
 // }
 
 //coding challenge 5
+
+// var john = {
+// 	fullName: 'John Smith',
+// 	bills: [124, 48, 268, 180, 42],
+// 	calcTips: function() {
+// 		this.tips = [];
+// 		this.finaleValues = [];
+
+// 		for (var i = 0; i < this.bills.length; i++)
+// 		{
+// 			//determine percentage based on tiping rules
+// 			var percentage;
+// 			var bill = this.bills[i]
+
+// 			if(bill < 50) {
+// 				percentage = .2;
+// 			} else if (bill >= 50 && bill < 200) {
+// 					percentage = .15;
+// 				} else {
+// 					pecentage = .1;
+// 				}
+// 			//add results to the arrays
+// 			this.tips[i] = bill * percentage;
+// 			this.finaleValues[i] = bill + bill * percentage;
+// 		}
+// 	}
+// }
+
+// var mark = {
+// 	fullName: 'Mark Miller',
+// 	bills: [77, 475, 110, 45],
+// 	calcTips: function() {
+// 		this.tips = [];
+// 		this.finaleValues = [];
+
+// 		for (var i = 0; i < this.bills.length; i++)
+// 		{
+// 			//determine percentage based on tiping rules
+// 			var percentage;
+// 			var bill = this.bills[i]
+
+// 			if(bill < 100) {
+// 				percentage = .2;
+// 			} else if (bill >= 100 && bill < 300) {
+// 					percentage = .15;
+// 				} else {
+// 					pecentage = .25;
+// 				}
+// 			//add results to the arrays
+// 			this.tips[i] = bill * percentage;
+// 			this.finaleValues[i] = bill + bill * percentage;
+// 		}
+// 	}
+// }
+
+// function calcAverage(tips) {
+// 	var sum = 0;
+// 	for (var i = 0; i < tips.length; i++) {
+// 		sum = sum + tips[i];
+// 	}
+// 	return sum / tips.length;
+// }
+
+// //math
+
+// john.calcTips();
+// mark.calcTips();
+// console.log(john, mark);
+
+// john.average = calcAverage(john.tips);
+// mark.average = calcAverage(mark.tips);
+// console.log(john, mark);
+
+// if (john.average > mark.average) {
+// 	console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+// } else if (mark.average > john.average) {
+// 	console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+// }
+
